048d2078684d391ed14369170f328c0de88e500e
itahamed20-droid <itahamed20@gmail.com>
2026-02-08 13:27:04 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2, num3;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    cout << "Enter third number: ";
+    cin >> num3;
+
+    int result = num1 + num2 + num3;
+
+    cout << num1 << " + " << num2 << " + " << num3 << " = " << result << endl;
+
+    return 0;
+}
